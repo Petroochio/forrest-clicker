d509d1a45a1e4682d40e99612d27f854e7cb48e3
Peter Gyory <ptg3662@rit.edu>
2016-03-25 15:59:40 -0400

    Fix path?

--- index.js
+++ index.js
@@ -8,16 +8,9 @@ var app = express();
 var port = process.env.PORT || 3000;
 app.set('port', port);
 
-app.use('/', express.static(path.join(__dirname, 'build/')));
+app.use('/', express.static(path.resolve('./build/')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.get('/account.json', function(req, res) {
-  fs.readFile('account.json', function(err, data) {
-    res.setHeader('Cache-Control', 'no-cache');
-    res.json(JSON.parse(data));
-  });
-});
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
